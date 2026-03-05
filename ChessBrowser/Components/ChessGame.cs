2f7aeb4cac02b6353c7126670d7f5d1045373f9b
Jose Ojeda <joseojeda@edi-cesa-909966.ad.utah.edu>
2026-03-05 16:26:25 -0700

    chessGame getters/setter

--- ChessBrowser/Components/ChessGame.cs
+++ ChessBrowser/Components/ChessGame.cs
@@ -1,6 +1,17 @@
 ﻿namespace ChessBrowser.Components
+#nullable disable
 {
     public class ChessGame
     {
+        public string EventName { get; set; }
+        public string Site { get; set; }
+        public DateTime EventDate { set; get; }
+        public string Round { get; set; }
+        public string WhitePlayer { get; set; }
+        public string BlackPlayer { get; set; }
+        public int WhiteElo { set; get; }
+        public int BlackElo { set; get; }
+        public char Result { set; get; }
+        public string Moves { set; get; }
     }
 }
