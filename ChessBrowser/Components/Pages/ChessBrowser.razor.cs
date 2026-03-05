c18b696970a80e1c3a9cfa22202e2cf79403c764
Jose Ojeda <joseojeda@joses-air.lan>
2026-03-05 15:01:47 -0700

    testing repo

--- ChessBrowser/Components/Pages/ChessBrowser.razor.cs
+++ ChessBrowser/Components/Pages/ChessBrowser.razor.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
 using System.Diagnostics;
 using MySql.Data.MySqlClient;
-
+// testing repo
 namespace ChessBrowser.Components.Pages
 {
   public partial class ChessBrowser
